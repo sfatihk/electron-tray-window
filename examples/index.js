62b70de0c877787f0798b47f7e620c03fd2654a2
Seyyid Fatih KOÇ <sakkarmen@gmail.com>
2018-12-03 22:56:59 +0300

    Created example project

---
+++ examples/index.js
@@ -0,0 +1,13 @@
+const TrayWindow = require("electron-tray-window");
+
+const { ipcMain, Tray, app, BrowserWindow } = require("electron");
+const path = require("path");
+
+app.on("ready", () => {
+  TrayWindow.setOptions({
+    trayIconPath: path.join("resources/icon.png"),
+    windowUrl: `file://${path.join(__dirname, "resources/index.html")}`,
+    width: 340,
+    height: 380
+  });
+});
