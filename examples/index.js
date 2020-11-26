fab42fa0087dcb429cdd73e130eb0717682b833d
飞扬 <ycrao@users.noreply.github.com>
2020-11-26 13:03:55 +0800

    for linux transparent, need wait at least 100 ms (depends on your machine, 20 ms maybe ok)

--- examples/index.js
+++ examples/index.js
@@ -4,10 +4,12 @@ const { ipcMain, Tray, app, BrowserWindow } = require("electron");
 const path = require("path");
 
 app.on("ready", () => {
-  TrayWindow.setOptions({
-    trayIconPath: path.join("resources/icon.png"),
-    windowUrl: `file://${path.join(__dirname, "resources/index.html")}`,
-    width: 340,
-    height: 380
-  });
+  setTimeout(function () {
+    TrayWindow.setOptions({
+      trayIconPath: path.join("resources/icon.png"),
+      windowUrl: `file://${path.join(__dirname, "resources/index.html")}`,
+      width: 340,
+      height: 380
+    });
+  }, 100);
 });
